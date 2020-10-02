eb68d7aba180629f10dee473a988d070f22de525
rukundoeric <45217364+rukundoeric@users.noreply.github.com>
2020-10-03 00:04:46 +0200

    Add deals section & footer

--- js/index.js
+++ js/index.js
@@ -1,4 +1,28 @@
 $(document).ready(() => {
+  $(window).scroll(() => {
+    const windowTop = $(window).scrollTop() + 1;
+    const windowWidth = $(window).width();
+    if(windowWidth > 800) {
+      if (windowTop > 50) {
+        $("#nav_bar_menu").addClass("fixed");
+        $("#ntw_logo").css("display", "flex");
+        $("#nav_bar_sub_links").css("display", "none");
+      } else {
+        $("#nav_bar_menu").removeClass("fixed");
+        $("#ntw_logo").css("display", "none");
+        $("#nav_bar_sub_links").css("display", "flex");
+      }
+    } else if (windowWidth < 764) {
+       $("#ntw_logo").css("display", "flex");
+       if (windowTop > 50) {
+         $("#nav_bar_menu").addClass("fixed");
+         $("#nav_bar_sub_links").css("display", "none");
+       } else {
+         $("#nav_bar_menu").removeClass("fixed");
+         $("#nav_bar_sub_links").css("display", "none");
+       }
+    }
+  });
   $("#open-menu-btn").on("click", () => {
     if ($("#sm-screen-menu-dir").hasClass("close_menu")) {
       $("#sm-screen-menu-dir").removeClass("close_menu");
